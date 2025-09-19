e46d5bf1c58fce2c480bc45dcf26b95498b3f971
RiderMC126 <ridermc123@gmail.com>
2025-09-19 23:54:05 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-from fastapi import FastAPI, requests, Response, WebSocket
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, HTTPException, Depends, UploadFile, File, WebSocket
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse, StreamingResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+import logging
+import sys
+from config import *
+from utils import *
+from db import *
 
+
+# Initialization application
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+
+# Main Page
+@app.get("/", response_class=HTMLResponse)
+async def mainPage(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request,
+         "title": "MainPage"}
+    )
+
+
+# Start application
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
